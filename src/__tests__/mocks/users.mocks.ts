b3e86b0a36ff75d5f1491c2fcef4f7071b81eb25
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-01-11 12:21:20 -0300

    feat: mock users

---
+++ src/__tests__/mocks/users.mocks.ts
@@ -0,0 +1,20 @@
+const mockedUserRequest = {
+    name: "testinho",
+    email: "testinho@mail.com",
+    password: "Tt123!@#",
+    birthdate: "08/17/2002",
+    CPF: "391.866.776-67"
+}
+
+const mockedUserLogin = {
+    email: "testinho@mail.com",
+    password: "Tt123!@#"
+}
+
+const mockedUserWrongCPF = {
+    name: "testinho2",
+    email: "testinho2@mail.com",
+    password: "Tt123!@#",
+    birthdate: "08/17/2002",
+    CPF: "111.222.333-07"
+}
