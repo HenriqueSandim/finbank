7d62e78967b37da24366209a3d83d34ac76ae41f
Jalles <jallessantos12@gmail.com>
2023-01-11 12:35:11 -0300

    fix: change response status code from schemaValidade.middleware

--- src/middlewares/schemaValidate.middleware.ts
+++ src/middlewares/schemaValidate.middleware.ts
@@ -1,21 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { AnySchema } from "yup";
 
-const schemaValidate =
-    (schema: AnySchema) =>
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const validatedData = await schema.validate(req.body, {
-                abortEarly: false,
-                stripUnknown: true,
-            });
+const schemaValidate = (schema: AnySchema) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const validatedData = await schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
 
-            req.body = validatedData;
+    req.body = validatedData;
 
-            return next();
-        } catch (err: any) {
-            return res.status(401).json({ message: err.errors });
-        }
-    };
+    return next();
+  } catch (err: any) {
+    return res.status(400).json({ message: err.errors });
+  }
+};
 
 export default schemaValidate;
