c36e4b52eed8c71f13b2ece4accea70b3e41012a
Jalles <jallessantos12@gmail.com>
2023-01-13 09:55:24 -0300

    fix: update serializers function type and add validation test for finances.serializers

--- src/serializers/finances.serializers.ts
+++ src/serializers/finances.serializers.ts
@@ -5,13 +5,16 @@ import { ICategoryResponse } from "../interfaces/categories.interfaces";
 import { IFinanceRequest, IFinanceResponse } from "../interfaces/finances.interfaces";
 
 const categorySchema: SchemaOf<ICategoryResponse> = yup.object().shape({
-  name: yup.string().required(),
-  id: yup.string().required(),
+  name: yup.string(),
+  id: yup.string(),
+});
+const validCategorySchema = categorySchema.test("one-required", "category name or id is required", (value) => {
+  return !!(value.name || value.id);
 });
 
 export const createFinanceSchema: SchemaOf<IFinanceRequest> = yup.object().shape({
   description: yup.string().max(150).required(),
   value: yup.number().required(),
   isIncome: yup.boolean().required(),
-  category: yup.array().of(categorySchema).required(),
+  category: yup.array().of(validCategorySchema).required(),
 });
