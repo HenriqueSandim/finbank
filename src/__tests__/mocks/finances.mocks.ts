664aeebff4cdfa04a132ca15ef286943ba40ebfe
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-01-15 10:26:15 -0300

    feat: finances tests in progress

---
+++ src/__tests__/mocks/finances.mocks.ts
@@ -0,0 +1,10 @@
+import Account from "../../entities/account.entity";
+
+export const mockedFinance = {
+    description: "Finance test",
+    value: 1000,
+    isIncome: true,
+    account: 0,
+    financesCategory: [{name: "Salario"}],
+    createdAt: "2023/01/14"
+}
