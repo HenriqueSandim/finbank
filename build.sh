52d35b6eeb145039eb4a0b96b4e560c48ff41bc4
Jalles <jallessantos12@gmail.com>
2023-01-18 10:54:46 -0300

    fix: trying to install chrome in linux

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
 yarn 
 yarn build
 yarn typeorm migration:run -d dist/src/data-source
