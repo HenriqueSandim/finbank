953f548d23b3283142436d60544c02d501e45838
luis <luisgabriel.modena326@gmail.com>
2023-01-18 09:17:17 -0300

    fixing arq

--- src/middlewares/balance/userBalance.service.ts
+++ src/middlewares/balance/userBalance.service.ts
@@ -1,12 +1,18 @@
 import AppDataSource from "../../data-source";
 import Account from "../../entities/account.entity";
 
+<<<<<<< Updated upstream:src/middlewares/balance/userBalance.service.ts
 const userBalanceService = async (userAccount: string) => {
   const balance = await AppDataSource.createQueryBuilder()
     .select(["account.money"])
     .from(Account, "account")
     .where("id = :account", { account: userAccount })
     .getOne();
+=======
+const userBalanceService = async (userAccount: number): Promise<IBalanceResponse> => {
+  const accountRepository = AppDataSource.getRepository(Account)
+  const balance = await accountRepository.findOneBy({id: userAccount})
+>>>>>>> Stashed changes:src/services/balance/userBalance.service.ts
 
   return { balance: balance.money };
 };
